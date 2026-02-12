f4f76b852e1b04535efef0c671b1e22aed5d31b8
GSaiCharanTeja <gavidisaicharanteja@gmail.com>
2026-02-12 20:51:16 +0530

    Add fields and getters/setters to Book class

--- src/main/java/com/example/SkillExperiement6/Book.java
+++ src/main/java/com/example/SkillExperiement6/Book.java
@@ -1,5 +1,17 @@
 package com.example.SkillExperiement6;
-
 public class Book {
-
+		int bno;
+		String bname;
+		public int getBno() {
+			return bno;
+		}
+		public void setBno(int bno) {
+			this.bno = bno;
+		}
+		public String getBname() {
+			return bname;
+		}
+		public void setBname(String bname) {
+			this.bname = bname;
+		}
 }
